0304c4a572e3b58576801840d310af54e0961670
kaarthik017 <kaarthik017@gmail.com>
2021-05-29 17:29:00 +0530

    Commit PORT update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dbUrl = process.env.DATABASE_ACCESS
   
 let objectId = mongodb.ObjectID;
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
